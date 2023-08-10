b64b519981cf5cd1ccf4947db61e346e68e9b0b7
deryadok <dokderya@gmail.com>
2023-08-10 21:20:36 +0300

    Add project files.

---
+++ CovidChart.API/Models/Covid.cs
@@ -0,0 +1,19 @@
+﻿namespace CovidChart.API.Models
+{
+    public enum CityEnum
+    {
+        Istanbul = 1,
+        Ankara = 2,
+        Izmır = 3,
+        Antalya = 4,
+        Ordu = 5
+    }
+
+    public class Covid
+    {
+        public int Id { get; set; }
+        public CityEnum City { get; set; }
+        public int Count { get; set; }
+        public DateTime CovidDate { get; set; }
+    }
+}
